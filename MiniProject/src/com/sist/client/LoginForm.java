65ff1c97ef62ced5c2f8bdedcd864feb6c8c288e
KIM MINSEOK <mindory144@gmail.com>
2025-08-14 17:22:24 +0900

    버튼 및 페이지 기본틀 제작 (2025-08-14)

---
+++ MiniProject/src/com/sist/client/LoginForm.java
@@ -0,0 +1,8 @@
+package com.sist.client;
+import javax.swing.*;
+import java.awt.*;
+public class LoginForm extends JPanel {
+	public LoginForm() {
+		setBackground(Color.white);
+	}
+}
---
+++ MiniProject/src/com/sist/client/SearchForm.java
@@ -0,0 +1,8 @@
+package com.sist.client;
+import javax.swing.*;
+import java.awt.*;
+public class SearchForm extends JPanel {
+	public SearchForm() {
+		setBackground(Color.gray);
+	}
+}

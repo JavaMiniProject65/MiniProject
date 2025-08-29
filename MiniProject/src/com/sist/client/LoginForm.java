4c5edb86be815301bb7f162e8fb4d5a260e458b1
KIM MINSEOK <mindory144@gmail.com>
2025-08-29 17:26:04 +0900

    <김민석> : 로그인 JFrame 완료

--- MiniProject/src/com/sist/client/LoginForm.java
+++ MiniProject/src/com/sist/client/LoginForm.java
@@ -1,8 +1,39 @@
 package com.sist.client;
 import javax.swing.*;
 import java.awt.*;
-public class LoginForm extends JPanel {
+public class LoginForm extends JFrame {
+	JLabel la1,la2,la3;
+	JTextField tf;
+	JPasswordField pf;
+	JButton b1,b2;
 	public LoginForm() {
-		setBackground(Color.white);
+		// 초기화 / 배치
+		setLayout(null);
+		la1=new JLabel("ID",JLabel.CENTER);
+		la2=new JLabel("PW",JLabel.CENTER);
+		la3=new JLabel("LOGIN",JLabel.CENTER);
+		tf=new JTextField();
+		pf=new JPasswordField();
+		b1=new JButton("로그인");
+		b2=new JButton("취소");
+		
+		la1.setBounds(60, 75, 60, 30);
+		tf.setBounds(125, 75, 150, 30);
+		add(la1);add(tf);
+		la2.setBounds(60, 110, 60, 30);
+		pf.setBounds(125, 110, 150, 30);
+		add(la2);add(pf);
+		la3.setBounds(160, 30, 60, 30);
+		add(la3);
+		
+		JPanel p=new JPanel();
+		p.add(b1);p.add(b2);
+		p.setBounds(15, 150, 365, 35);
+		add(p);
+		setSize(400, 250);
+		setVisible(true);       
+	}
+	public static void main(String[] args) {
+		new LoginForm();
 	}
 }
